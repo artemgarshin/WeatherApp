8a16c2e0e64bc0d36eaa20a273ffb21c2b084eff
artemgarshin <garshinareem@gmail.com>
2023-09-22 12:45:35 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Артем Гаршин on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
